babe83fb9e0e3d12936e5b479b96d6078cf49377
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-13 17:25:36 +0100

    created game and team python classes

---
+++ models/game.py
@@ -0,0 +1,7 @@
+class Game():
+    def __init__(self, team1, team2, team1_goals, team2_goals, id = None):
+        self.team1 = team1
+        self.team2 = team2
+        self.team1_goals = team1_goals
+        self.team2_goals = team2_goals
+        self.id = id
---
+++ models/team.py
@@ -0,0 +1,5 @@
+class Team():
+    def __init__(self, name, manager, id = None):
+        self.name = name
+        self.manager = manager
+        self.id = id
