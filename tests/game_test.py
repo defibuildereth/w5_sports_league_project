404c13a06633a4c9be4c6e66f86c26fe2e414c7f
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-13 17:44:00 +0100

    setup TestGame, game has team test passed

--- tests/game_test.py
+++ tests/game_test.py
@@ -1,11 +1,13 @@
-# import unittest
-# from models.game import Game
-# from models.team import Team
+import unittest
+from models.game import Game
+from models.team import Team
 
-# class TestGame(unittest.TestCase):
+class TestGame(unittest.TestCase):
 
-#     def setUp(self):
-#         self.team1 = Team("ICTFC", "Tapestry")
-#         self.team2 = Team("HMFC", "Robbo")
-#         self.game = Game(self.team1, self.team2, 4, 0)
+    def setUp(self):
+        self.team1 = Team("ICTFC", "Tapestry")
+        self.team2 = Team("HMFC", "Robbo")
+        self.game = Game(self.team1, self.team2, 4, 0)
 
+    def test_game_has_team1(self):
+        self.assertEqual("ICTFC", self.game.team1.name)
