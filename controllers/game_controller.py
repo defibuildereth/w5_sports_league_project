bbb362ecc0c2f6d34f52240f1dec4793f260eafe
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-16 13:17:15 +0100

    new game form works :O

--- controllers/game_controller.py
+++ controllers/game_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, Blueprint
 
 import repositories.game_repository as game_repository
 import repositories.team_repository as team_repository
+from models.game import Game
 
 games_blueprint = Blueprint("games", __name__)
 
@@ -14,3 +15,15 @@ def games():
 def new_game():
     teams = team_repository.select_all()
     return render_template("/games/new.html", teams = teams)
+
+@games_blueprint.route("/games", methods=["POST"])
+def add_game():
+    team1_id = request.form['team1']
+    team1 = team_repository.select(team1_id)
+    team2_id = request.form['team2']
+    team2 = team_repository.select(team2_id)
+    team1_goals = request.form['team1_goals']
+    team2_goals = request.form['team2_goals']
+    game = Game(team1, team2, team1_goals, team2_goals)
+    game_repository.save(game)
+    return redirect("/games")
