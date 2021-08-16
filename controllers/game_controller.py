c72c4cfd20eaa9d2d490c622c004c892e8a1b2b8
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-16 11:18:17 +0100

    games route/index setup

--- controllers/game_controller.py
+++ controllers/game_controller.py
@@ -1,3 +1,10 @@
 from flask import Flask, render_template, request, redirect, Blueprint
 
+import repositories.game_repository as game_repository
+
 games_blueprint = Blueprint("games", __name__)
+
+@games_blueprint.route("/games")
+def games():
+    games = game_repository.select_all()
+    return render_template("/games/index.html", games = games)
