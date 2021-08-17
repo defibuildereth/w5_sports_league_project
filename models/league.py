3b183f0c649973b915c48cb6ef3094301f8ad7c2
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-17 12:20:12 +0100

    league get_game_points works

--- models/league.py
+++ models/league.py
@@ -1,6 +1,7 @@
 import repositories.team_repository as team_repository
 import repositories.game_repository as game_repository
 
+
 class League():
     def __init__(self, teams = [], games = []):
         self.teams = teams
@@ -14,8 +15,21 @@ class League():
         games = game_repository.select_all()
         return games
 
-    def get_games_played(self, id):
-        return len(game_repository.games(id))
+    def get_games_played(self, team_id):
+        return len(game_repository.games(team_id))
+
+    def get_game_points(self, game_id, team_id):
+        
+        game = game_repository.select(game_id)
+        if game.team1_goals == game.team2_goals:
+            points = 1
+        elif game.team1.id == team_id:
+            if game.team1_goals > game.team2_goals:
+                points = 3
+        else:
+            points = 0
+        return points
+
 
 
     # def full_league():
